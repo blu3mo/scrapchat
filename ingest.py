fdafb294fc3f7dafdc40bc82b57880d3b493188a
Bluemo <shutaro.aoyama@gmail.com>
2023-03-03 19:30:12 -0500

    Build ScrapboxLoader

--- ingest.py
+++ ingest.py
@@ -6,23 +6,30 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 
+from scrapchat.ScrapboxLoader import ScrapboxLoader
+
 
 def ingest_docs():
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
+    loader = ScrapboxLoader("keidaroo-blu3mo.json")
     raw_documents = loader.load()
+    print(raw_documents)
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
     )
     documents = text_splitter.split_documents(raw_documents)
-    embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.from_documents(documents, embeddings)
+    print(documents)
+    #embeddings = OpenAIEmbeddings()
+    #vectorstore = FAISS.from_documents(documents, embeddings)
 
     # Save vectorstore
-    with open("vectorstore.pkl", "wb") as f:
-        pickle.dump(vectorstore, f)
+    #with open("vectorstore.pkl", "wb") as f:
+        #pickle.dump(vectorstore, f)
 
+# def ingest_scrapbox():
+#     return
+#
 
 if __name__ == "__main__":
     ingest_docs()
