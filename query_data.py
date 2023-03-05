0508f842e001aab8265aac57d2721c055e167d1c
Bluemo <shutaro.aoyama@gmail.com>
2023-03-05 15:13:03 -0500

    Improve template

--- query_data.py
+++ query_data.py
@@ -1,4 +1,5 @@
 """Create a ChatVectorDBChain for question/answering."""
+from langchain import PromptTemplate
 from langchain.callbacks.base import AsyncCallbackManager
 from langchain.callbacks.tracers import LangChainTracer
 from langchain.chains import ChatVectorDBChain
@@ -41,8 +42,20 @@ def get_chain(
     question_generator = LLMChain(
         llm=question_gen_llm, prompt=CONDENSE_QUESTION_PROMPT, callback_manager=manager
     )
+
+    flexible_qa_template = """You are a smart assistant. Answer the question at the end.
+    Context:
+    {context}
+    
+    If any of the above is not relevant, you can ignore it.
+    Question: {question}
+    Helpful Answer:"""
+    FLEXIBLE_QA_PROMPT = PromptTemplate(
+        template=flexible_qa_template,
+        input_variables=["context", "question"]
+    )
     doc_chain = load_qa_chain(
-        streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
+        streaming_llm, chain_type="stuff", prompt=FLEXIBLE_QA_PROMPT, callback_manager=manager
     )
 
     qa = ChatVectorDBChain(
